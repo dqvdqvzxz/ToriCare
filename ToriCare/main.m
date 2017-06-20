e45da25eb2e109e92dac22220947af06b0fcd151
MeoBeo <cryicestar92@gmail.com>
2017-06-20 08:51:01 +0700

    First Commit

---
+++ ToriCare/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ToriCare
+//
+//  Created by Mèo Béo on 6/20/17.
+//  Copyright © 2017 Mèo Béo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
