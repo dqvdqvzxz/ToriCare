0af44ba521d4b814a644da35ccc11d70a21a4844
MeoBeo <cryicestar92@gmail.com>
2017-06-20 11:29:17 +0700

    Add define String

---
+++ ToriCare/Global/Constant/TRCDefine_String.h
@@ -0,0 +1,14 @@
+//
+//  TRCDefine_String.h
+//  ToriCare
+//
+//  Created by Mèo Béo on 6/20/17.
+//  Copyright © 2017 Mèo Béo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "TRCDefine_Config.h"
+
+//title
+#define kTitleHome          LocalizedString(@"Home")
