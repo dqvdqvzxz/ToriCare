258e6c6c055d8e8030dd6ed93d19f7808b65fb5b
MeoBeo <cryicestar92@gmail.com>
2017-06-20 10:05:23 +0700

    Add extension UiViewController

---
+++ ToriCare/Global/Extensions/UIViewController+Extension.h
@@ -0,0 +1,17 @@
+//
+//  UIViewController+Extension.h
+//  ToriCare
+//
+//  Created by Mèo Béo on 6/20/17.
+//  Copyright © 2017 Mèo Béo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIViewController (Extension)
+
+-(void)customBackButton;
+
+-(void)setCustomTitle:(NSString *)title;
+
+@end
---
+++ ToriCare/Global/Extensions/UIViewController+Extension.m
@@ -0,0 +1,38 @@
+//
+//  UIViewController+Extension.m
+//  ToriCare
+//
+//  Created by Mèo Béo on 6/20/17.
+//  Copyright © 2017 Mèo Béo. All rights reserved.
+//
+
+#import "UIViewController+Extension.h"
+
+@implementation UIViewController (Extension)
+
+-(void)setCustomTitle:(NSString *)title{
+    NSDictionary *attributes = @{NSForegroundColorAttributeName : [UIColor whiteColor],
+                                 NSFontAttributeName : [UIFont systemFontOfSize:20.0f]};
+    NSMutableAttributedString *titleString = [[NSMutableAttributedString alloc] initWithString:title attributes:attributes];
+    UILabel *titleLabel = [UILabel new];
+    titleLabel.attributedText = titleString;
+    [titleLabel sizeToFit];
+    self.navigationItem.titleView = titleLabel;
+}
+
+-(void)customBackButton{
+    UIButton *customBackButton = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 25, 25)];
+    customBackButton.multipleTouchEnabled = NO;
+    customBackButton.exclusiveTouch = YES;
+    [customBackButton setImage:[UIImage imageNamed:@"btn_back"] forState:UIControlStateNormal];
+    customBackButton.contentEdgeInsets = UIEdgeInsetsMake(0, -10, 0, 10);
+    [customBackButton addTarget:self action:@selector(customPopViewController) forControlEvents:UIControlEventTouchUpInside];
+    UIBarButtonItem *customBack = [[UIBarButtonItem alloc] initWithCustomView:customBackButton];
+    self.navigationItem.leftBarButtonItem = customBack;
+}
+
+-(void)customPopViewController{
+    [self.navigationController popViewControllerAnimated:YES];
+}
+
+@end
