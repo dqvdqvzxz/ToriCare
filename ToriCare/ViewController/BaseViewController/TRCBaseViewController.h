c87193ced682dec2455c6594d559bf8de0190d41
MeoBeo <cryicestar92@gmail.com>
2017-06-20 09:17:18 +0700

    Create BaseViewController and Login folder

---
+++ ToriCare/ViewController/BaseViewController/TRCBaseViewController.h
@@ -0,0 +1,13 @@
+//
+//  TRCBaseViewController.h
+//  ToriCare
+//
+//  Created by Mèo Béo on 6/20/17.
+//  Copyright © 2017 Mèo Béo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TRCBaseViewController : NSObject
+
+@end
---
+++ ToriCare/ViewController/BaseViewController/TRCBaseViewController.m
@@ -0,0 +1,13 @@
+//
+//  TRCBaseViewController.m
+//  ToriCare
+//
+//  Created by Mèo Béo on 6/20/17.
+//  Copyright © 2017 Mèo Béo. All rights reserved.
+//
+
+#import "TRCBaseViewController.h"
+
+@implementation TRCBaseViewController
+
+@end
